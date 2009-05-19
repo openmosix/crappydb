85639391353f35c5d685f90a4362e84f809a6ceb
luca.bonmassar <luca.bonmassar@290333f0-3fd1-11de-8fef-1f5ae417a2df>
2009-05-19 23:43:27 +0000

    Added data types - types used in the storage layer

---
+++ src/main/java/org/bonmassar/crappydb/server/storage/data/Item.java
@@ -0,0 +1,55 @@
+/*
+ *  This file is part of CrappyDB-Server, 
+ *  developed by Luca Bonmassar <luca.bonmassar at gmail.com>
+ *
+ *  CrappyDB-Server is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CrappyDB-Server is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CrappyDB-Server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.bonmassar.crappydb.server.storage.data;
+
+import java.io.Serializable;
+
+public class Item {
+
+	private Key	storagekey;
+	private Integer flags;
+	private Serializable data;
+	private Cas internalcas;
+	
+	public Item(Key storagekey, Serializable data){
+		init(storagekey, data, null);
+	}
+	
+	public Item(Key storagekey, Serializable data, Integer flags){
+		init(storagekey, data, flags);
+	}
+	
+	public Item(Serializable data){
+		init(null, data, null);
+	}
+	
+	public Item(Serializable data, Integer flags){
+		init( null, data, flags );
+	}
+	
+	public void setCas(Cas newcas){
+		this.internalcas = newcas;
+	}
+	
+	private void init(Key storagekey, Serializable data, Integer flags){
+		this.data = data;
+		this.flags = flags;
+		this.storagekey = storagekey;
+	}
+}
---
+++ src/main/java/org/bonmassar/crappydb/server/storage/data/Key.java
@@ -0,0 +1,69 @@
+/*
+ *  This file is part of CrappyDB-Server, 
+ *  developed by Luca Bonmassar <luca.bonmassar at gmail.com>
+ *
+ *  CrappyDB-Server is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CrappyDB-Server is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CrappyDB-Server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.bonmassar.crappydb.server.storage.data;
+
+public class Key {
+	
+	/**
+	 * Maximum size for an item key as defined by memcache protocol.
+	 * See 'Keys' in http://code.sixapart.com/svn/memcached/trunk/server/doc/protocol.txt
+	 */
+	private final int MAX_KEY_SIZE=250;
+
+	private String value;
+	
+	public Key(String value){
+		importKeyValue(value);
+	}
+	
+	public String toString() {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object dest) {
+		if(null == dest || !(dest instanceof Key))
+			return false;
+
+		return value.equals(((Key)dest).value);
+	}
+
+	private void importKeyValue(String value) {
+		this.value = value;
+		checkInvalidKey();
+		cleanupKey();
+		truncateKeyIfLongerMaximumSize(); 
+	}
+
+	private void cleanupKey() {
+		value = value.trim();
+	}
+
+	private void checkInvalidKey() {
+		if(null == value || 0 == value.trim().length())
+			throw new IllegalArgumentException("Storage key cannot be null.");	
+	}
+	
+	private void truncateKeyIfLongerMaximumSize() {
+		if(value.length() <= MAX_KEY_SIZE)
+			return;
+		
+		value = value.substring(0, MAX_KEY_SIZE);
+	}
+}
