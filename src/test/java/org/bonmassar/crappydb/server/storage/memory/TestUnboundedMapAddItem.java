2da5993c5fda538b480af085a1c6eabe49d7c4b6
luca.bonmassar <luca.bonmassar@290333f0-3fd1-11de-8fef-1f5ae417a2df>
2009-05-21 17:30:18 +0000

    Tests for add items to unbounded map

---
+++ src/test/java/org/bonmassar/crappydb/server/storage/memory/TestUnboundedMapAddItem.java
@@ -0,0 +1,79 @@
+/*
+ *  This file is part of CrappyDB-Server, 
+ *  developed by Luca Bonmassar <luca.bonmassar at gmail.com>
+ *
+ *  CrappyDB-Server is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CrappyDB-Server is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CrappyDB-Server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.bonmassar.crappydb.server.storage.memory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.bonmassar.crappydb.server.exceptions.NotStoredException;
+import org.bonmassar.crappydb.server.exceptions.StorageException;
+import org.bonmassar.crappydb.server.storage.data.Item;
+import org.bonmassar.crappydb.server.storage.data.Key;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUnboundedMapAddItem {
+
+	private UnboundedMap um;
+	
+	@Before
+	public void setUp(){
+		um = new UnboundedMap();
+	}
+	
+	@Test
+	public void testNullObject() {
+		try {
+			um.add(null);
+		} catch (NotStoredException e) {
+			assertEquals("NotStoredException [Null item]", e.toString());
+		} catch (StorageException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testInvalidKey() {
+		try {
+			Item it = new Item (null, "some data");
+			um.add(it);
+		} catch (NotStoredException e) {
+			assertEquals("NotStoredException [Invalid key]", e.toString());
+		} catch (StorageException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testKeyAdded() {
+		try {
+			Key k = new Key("Yuppi");
+			Item it = new Item (k, "some data");
+			um.add(it);
+			assertEquals(1, um.repository.size());
+			assertEquals(it, um.repository.get(k));
+			assertNotNull(um.repository.get(k).getCas());
+			assertTrue(um.repository.get(k).getCas().toString().length() > 0);
+		} catch (Exception e) {
+			fail();
+		}
+	}
+}
