63e09080b7bff122ae42d42c265eb1bff325c68d
luca.bonmassar <luca.bonmassar@290333f0-3fd1-11de-8fef-1f5ae417a2df>
2009-07-15 19:58:21 +0000

    removed warnings

--- src/test/java/org/bonmassar/crappydb/server/io/TestCyclicCountDownLatch.java
+++ src/test/java/org/bonmassar/crappydb/server/io/TestCyclicCountDownLatch.java
@@ -23,7 +23,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
-import org.bonmassar.crappydb.server.ShutdownExecutionRegister.Registry;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
